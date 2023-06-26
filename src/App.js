bb0a0cf4f29ab346945f2852b8f0a66d62de4f5c
Károly András Kovács <karoly.kovacs@greenfoxacademy.com>
2023-06-26 11:07:39 +0200

    feat(): add todo api call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function Todo({ id, name }) {
+  return <p>{name}</p>;
+}
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        'https://todo-backend-production-97bd.up.railway.app/todos'
+      );
+      const fetchedTodos = await response.json();
+      setTodos(fetchedTodos);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo app</h1>
+        {todos.map((todo, index) => {
+          return <Todo key={index} name={todo.name}></Todo>;
+        })}
       </header>
     </div>
   );
